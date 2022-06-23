cadf3fd790a73e9c97a0edda8d4f41e942643d4a
zephyrus21 <pandey.piyus@gmail.com>
2022-06-24 01:04:41 +0530

    added heders

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,18 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
+  if (req.nextUrl.pathname.startsWith("/api/get-url")) {
+    return;
+  }
+  const slug = req.nextUrl.pathname.split("/").pop();
+
+  const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+  if (slugFetch.status === 404) {
+    return NextResponse.redirect(req.nextUrl.origin);
+  }
+  const data = await slugFetch.json();
+
+  if (data?.url) {
+    return NextResponse.redirect(data.url);
+  }
+};
--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../db/client";
+import { prisma } from "../../../db/client";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -28,10 +28,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (!data) {
     res.statusCode = 404;
 
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Cache-Control",
+      "s-maxage=100000000000, stale-while-revalidate"
+    );
+
     res.send(JSON.stringify({ message: "slug not found" }));
 
     return;
   }
 
-  return res.redirect(data.url); // redirect to the original url
+  return res.json(data);
 };
