2ab0c06e2e734a02ad5f94fd78fced8ad1609fe8
zephyrus21 <pandey.piyus@gmail.com>
2022-06-24 01:36:35 +0530

    fixed '/' issue

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,7 +1,10 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
-  if (req.nextUrl.pathname.startsWith("/api/get-url")) {
+  if (
+    req.nextUrl.pathname.startsWith("/api/get-url") ||
+    req.nextUrl.pathname.startsWith("/")
+  ) {
     return;
   }
   const slug = req.nextUrl.pathname.split("/").pop();
