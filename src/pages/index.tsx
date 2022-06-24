15db0372ab2c101a146a8b3f5e2acdd8fed9ca15
zephyrus21 <pandey.piyus@gmail.com>
2022-06-24 17:07:54 +0530

    added UI and tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const CreateLinkForm = dynamic(() => import("components/CreateLinkForm"), {
+  ssr: false,
+});
 
 const Home: NextPage = () => {
-  return <div>Shorten your URL</div>;
+  return (
+    <div className='flex flex-col justify-center items-center h-screen bg-gray-900 text-white'>
+      <Suspense fallback={`Loading...`}>
+        <CreateLinkForm />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Home;
